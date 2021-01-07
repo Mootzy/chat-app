d52a6bbddf9dcd65e2595e742ba3f09c33838fa3
Mootzy <tyler77156@gmail.com>
2021-01-07 11:20:15 -0700

    boiler plate and init firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
+import React, {useEffect, useRef, useStat} from 'react';
+import firebase from 'firebase/app'
+import 'firebase/firestore';
+import 'firebase/auth';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import {useAuthState} from 'react-firebase-hooks/auth'
+import {useCollectionData} from 'react-firebase-hooks/firesore';
+
+firebase.initializeApp({
+
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+
+export default function App() {
+
+const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>
+        ⚛️🔥💬
+        </h1>
+        <SignOut />
       </header>
+
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
     </div>
   );
 }
 
-export default App;
+function SignOut() {
+
+  return auth.currentUser && (
+    <button onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+function SignIn() {
+
+  const signInWithGoogle = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  auth.SignInWithPopup(provider);
+  }
+  return (
+    <button onClick={signInWithGoogle}>Sign in with Google</button>
+  )
+}
+function ChatRoom() {
+
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limitedToLast(25);
+
+  const [messages] = useCollectionData(query, {idField: 'id' });
+
+
+}
+function ChatMessage() {}
